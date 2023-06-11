977f7184e06dcf08907bd01b7a4e1562986e7860
Rybnikov Maksim <r_max@outlook.com>
2023-06-11 23:13:55 +0300

    Delete ProductsAndCategories_1.sql

--- ProductsAndCategories_1.sql
+++
@@ -1,6 +0,0 @@
-use TestMindbox
-
-select Product.[name] as product, Category.[name] as category
-from
-	Product left join ProductCategories on Product.id = ProductCategories.product
-			left join Category on ProductCategories.category = Category.id
