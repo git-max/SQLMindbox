d0be290513ef62c9961ff477eedd29be4f17c77b
Rybnikov Maksim <r_max@outlook.com>
2023-06-11 23:14:26 +0300

    Delete ProductsAndCategories_2.sql

--- ProductsAndCategories_2.sql
+++
@@ -1,8 +0,0 @@
-use TestMindbox
-
-select prod.[name] as product, resCat.[name] as category
-from Product as prod
-	outer apply (
-		select * from ProductCategories as prodCat where prod.id = prodCat.product ) as a
-	outer apply (
-		select cat.[name] from Category as cat where cat.id = a.category ) as resCat
